03d91b4f8d69d07877cff8657486954a7805c9a7
isalf24 <jmiel5112@gmail.com>
2025-01-24 12:59:02 -0600

    Primer commit: subiendo mi proyecto

---
+++ run.py
@@ -0,0 +1,10 @@
+from todor import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
+
+
+# Agradezco la experiencia adquirida en este proyecto y en la actividad práctica de la Unidad 2.
+# Aunque entregamos tarde la tercera parte y no obtuvimos nota, adquirí conocimientos valiosos en estos temas.
+# - Isaias Medina
